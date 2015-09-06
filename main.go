6622921c48b30bc0cf727225f4f164b18e2e02b8
Santiago Lapresta <santiago.lapresta@gmail.com>
2015-09-06 23:31:24 +0200

    Ｉｄｅｏｇｒａｐｈｉｃ ｓｐａｃｅ ｉｓ ｂｅｓｔ ｓｐａｃｅ

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ func main() {
 	text := strings.Join(os.Args[1:], " ")
 
 	for _, c := range text {
-		if c >= 0x21 && c <= 0x7E {
+		if c == 0x20 {
+			fmt.Printf("%c", 0x3000)
+		} else if c >= 0x21 && c <= 0x7E {
 			fmt.Printf("%c", c+0xFEE0)
 		} else {
 			fmt.Printf("%c", c)
